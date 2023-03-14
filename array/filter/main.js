6d4afc67dff2ac1fcab398e922057d282941eacc
Emill <lemito66@gmail.com>
2023-03-14 18:07:59 -0500

    filter

---
+++ array/filter/main.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4,5,6,7,8];
+
+console.log(numbers.filter((pairs)=> pairs % 2 === 0));
+
+console.log(numbers.filter((moreOfThree)=> moreOfThree > 3));
+
+
+// Encontrar los nombres que empiezan con la letra "a"
+
+const nombres = ['Ana', 'Juan', 'Pedro', 'Antonio', 'Alberto', 'Andrea', 'Sofia', 'Alicia'];
+
+const finLetterA  = (collection, letter) => {
+    const upperLetter = letter.toUpperCase();
+    return collection.filter((name) => name[0].toUpperCase() === upperLetter);
+};
+
+console.log(finLetterA(nombres, 'a'));
