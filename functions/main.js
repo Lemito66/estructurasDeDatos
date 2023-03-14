5134737ba613c2766aad41c65c60b63d5576f377
Emill <lemito66@gmail.com>
2023-03-14 15:15:19 -0500

    Funciones

---
+++ functions/main.js
@@ -0,0 +1,11 @@
+const PI = 3.141592653589793;
+const d1 = 10;
+
+let result = PI * d1;
+console.log(result);
+
+function circlePerimeter(pi, diameter) {
+    return pi * diameter;
+}
+
+console.log(circlePerimeter(PI, d1));
