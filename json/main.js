592821df2c2a1be499d426c311369820689b16b4
Emill <lemito66@gmail.com>
2023-03-16 10:34:54 -0500

    api fetch

--- json/main.js
+++ json/main.js
@@ -5,9 +5,16 @@ const myPromise = new Promise((resolve, reject) => {
         console.log('Se ejecuta la promesa');
         reject("Error mi rey"); // Se ejecuta el catch
         resolve("pato"); // Se ejecuta el then
-    }, 3000);
+    }, 1000);
 });
 
 myPromise
     .then(data => console.log(`Se ejecuta la promesa ${data}`)) // Se ejecuta cuando invoco a resolve
     .catch(error => console.log(`Se ejecuta el error ${error}`)); // Se ejecuta cuando invoco a reject
+
+// Api fetch
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(data => data.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error));
