450e2ceadcf7d29d4041732e24edc17010f293d4
Emill <lemito66@gmail.com>
2023-03-17 09:16:45 -0500

    comentarios

--- queue/main.js
+++ queue/main.js
@@ -1,1 +1,14 @@
 // Implementación de la cola
+// Primero que entra, primero que sale (FIFO)
+class Queue {
+
+    #items = [];
+
+    isEmpty(){
+        return this.#items.length === 0;
+    }
+
+    peek(){
+        return this.#items[0]; // Devuelve el primer elemento de la cola
+    }
+}
