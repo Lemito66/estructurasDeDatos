48b43716755821bef9de044b8b246ac480e85d2a
Emill <lemito66@gmail.com>
2023-04-19 17:01:00 -0500

    método burbuja con javascript

---
+++ ordinationMethods/bubbleSort/bubbleSort.js
@@ -0,0 +1,22 @@
+// Método Burbuja
+
+const bubbleSort = (arrayOfNumbers) => {
+  let sorted = true;
+  let temp;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    for (let j = 0; j < arrayOfNumbers.length; j++) {
+      if (arrayOfNumbers[j] > arrayOfNumbers[j + 1]) {
+        sorted = false;
+        temp = arrayOfNumbers[j];
+        arrayOfNumbers[j] = arrayOfNumbers[j + 1];
+        arrayOfNumbers[j + 1] = temp;
+      }
+    }
+    if (sorted) {
+      break;
+    }
+  }
+  return arrayOfNumbers;
+};
+
+console.log(bubbleSort([5,4,3,2,1]));
