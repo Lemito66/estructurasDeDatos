ed64fb549ddbcf59640fa81023f6bedb9373ae3b
Emill <lemito66@gmail.com>
2023-05-05 10:56:15 -0500

    Usando nuestra API

--- json/asyncAwait.js
+++ json/asyncAwait.js
@@ -17,7 +17,7 @@ const namesApi = async () => {
   }
 };
 
-(async () => {
+/* (async () => {
   const result = await namesApi();
   for (const name of result) {
     console.log(name);
@@ -36,5 +36,25 @@ const namesApi = async () => {
     }
   } catch (error) {
     console.log(error);
+  }
+})(); */
+
+
+const apiDjango = async () => {
+  
+  try {
+    const url = await fetch("http://127.0.0.1:8000/tasks/api/v1/tasks/");
+    const data = await url.json();
+    return await data;
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+(async () => {
+  const result = await apiDjango();
+  const onlyId = result.map(({ id }) => `Id: ${ id }`);
+  for (const id of onlyId) {
+    console.log(id);
   }
 })();
