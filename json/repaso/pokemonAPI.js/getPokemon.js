1fbcba86b2b27c53e80c005fb203e36b58c11529
Emill <lemito66@gmail.com>
2023-05-18 11:56:37 -0500

    get con fetch

---
+++ json/repaso/pokemonAPI.js/getPokemon.js
@@ -0,0 +1,16 @@
+const getType = (id) => {
+  const data= [];
+  const options = { method: "GET" };
+
+  return fetch(`https://pokeapi.co/api/v2/type/${id}`, options)
+    .then((response) => response.json())
+    .then((response) => {
+        data.push(...response.damage_relations.double_damage_from);
+        return data;
+    })
+    .catch((err) => console.error(err));
+};
+
+getType(5)
+  .then((result) => console.log(result))
+  .catch((err) => console.error(err));
