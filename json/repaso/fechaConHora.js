1af0ba864923ba4fe328521b3a50f1bf35a26089
Emill <lemito66@gmail.com>
2023-05-23 21:19:49 -0500

    fecha y hora

---
+++ json/repaso/fechaConHora.js
@@ -0,0 +1,15 @@
+const fechaYHora = () => {
+    const fechaActual = new Date();
+    const dia = fechaActual.getDate();
+    const mes = fechaActual.getMonth() + 1;
+    const anio = fechaActual.getFullYear();
+
+    const hora = fechaActual.getHours();
+    const minutos = fechaActual.getMinutes();
+    const segundos = fechaActual.getSeconds();
+  
+    const fechaFormateada = `${dia}-${mes}-${anio} ${hora}:${minutos}:${segundos}`;
+    return fechaFormateada;
+}
+
+console.log(fechaYHora());
