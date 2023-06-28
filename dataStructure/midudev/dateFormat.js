8bb9405686576755001e45f0074dd3707d7489b9
Emill <lemito66@gmail.com>
2023-06-28 12:14:31 -0500

    formato fecha mejorado

---
+++ dataStructure/midudev/dateFormat.js
@@ -0,0 +1,16 @@
+const cargarFechaActual = () => {
+    const fechaActual = new Date();
+    const dia = fechaActual.getDate();
+    const mesNormal = fechaActual.getMonth() + 1;
+    const mes = mesNormal < 10 ? `0${mesNormal}` : mesNormal;
+    const anio = fechaActual.getFullYear();
+    const hora = fechaActual.getHours();
+    const minutosNormal = fechaActual.getMinutes();
+    const minutos = minutosNormal < 10 ? `0${minutosNormal}` : minutosNormal;
+
+    const fechaFormateada = `${dia}-${mes}-${anio} ${hora}:${minutos}`;
+    return fechaFormateada;
+    //console.log(FormularioDeRegistro.fechaActual);
+}
+
+console.log(cargarFechaActual());
